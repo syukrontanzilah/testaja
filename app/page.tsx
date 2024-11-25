2f1ae8170777ae91bc44a22a88e00789451e3ef4
syukrontanzilah <syukrontanzilah@gmail.com>
2024-11-26 06:40:19 +0700

    test 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="bg-black text-yellow-100 h-screen">
-      Test aja sih... test aja yaaa heheheh test aja ya wkwkkw
+      Test aja sih...
     </div>
   );
 }
