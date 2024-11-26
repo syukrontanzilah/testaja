45c313eba4920678af5a0d9cf7778d64f19b3cc6
syukrontanzilah <syukrontanzilah@gmail.com>
2024-11-27 05:31:04 +0700

    test aja

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 
 export default function Home() {
   return (
-    <div className="bg-gray-600 text-yellow-100 h-screen">
-      Test aja sih...ya deh
+    <div className="bg-gray-100 h-screen">
+      Test aja sih.. test ya test test <section></section> <div><span><span><strong>test aja ya</strong></span></span></div>
+      <img src="https://images.squarespace-cdn.com/content/v1/5b7c56e255b02c683659fe43/060970be-cb93-4867-9543-db9796a04020/GettyImages-671260408.jpg?format=2500w" alt="test-aja" className="h-40 w-auto rounded-xl"/>
     </div>
   );
 }
