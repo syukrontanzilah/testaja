422e90cc7fa119d4ad91a5374a3c8abd2823d31e
syukrontanzilah <syukrontanzilah@gmail.com>
2024-11-29 20:40:21 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,25 @@
 export default function Home() {
   return (
     <div className="bg-gray-100 h-screen">
-      Test aja sih.. test ya test test <section></section> <div><span><span><strong>test aja ya</strong>tes aja ya</span></span></div>
-      <img src="https://images.squarespace-cdn.com/content/v1/5b7c56e255b02c683659fe43/060970be-cb93-4867-9543-db9796a04020/GettyImages-671260408.jpg?format=2500w" alt="test-aja" className="h-40 w-auto rounded-xl"/>
+      Test aja sih.. test ya test test <section></section>{" "}
+      <div>
+        <span>
+          <span>
+            <strong>test aja ya</strong>tes aja ya
+          </span>
+        </span>
+      </div>
+      <img
+        src="https://images.squarespace-cdn.com/content/v1/5b7c56e255b02c683659fe43/060970be-cb93-4867-9543-db9796a04020/GettyImages-671260408.jpg?format=2500w"
+        alt="test-aja"
+        className="h-40 w-auto rounded-xl"
+      />
+      <strong>test aja ya</strong>
+      <div>test aja ya</div>
+      <div>test lagi saja</div>
+      <span>
+        <span className="text-purple-400">hahaha</span>
+      </span>
     </div>
   );
 }
